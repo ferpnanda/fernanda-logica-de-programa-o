aa33e8bb7f82a1e48018c277596cadddbb38f534
fer <alunafernandadepoliveira@gmail.com>
2024-09-18 11:22:48 -0300

    primeiro commit

---
+++ aula2.py
@@ -0,0 +1,8 @@
+nome = ('digite seu nome?')
+peso = input('qual é seu peso')
+print (nome,peso)
+
+nota1 = float(input('digite sua primeira nota'))
+nota2 = float(input('digite sua ultima nota'))
+media = (nota1= nota2)
+print ("A media das notas é:", media)
---
+++ variáveis_constantes.py
@@ -0,0 +1,6 @@
+ nome = 'Fernanda' 
+ idade= '15'
+
+
+ print ('nome')
+ print ('idade')
